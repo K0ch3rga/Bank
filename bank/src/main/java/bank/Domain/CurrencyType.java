574c8404ada7737a9eb4f76f27687772755ad247
Toby Rice <mr.skel@mail.ru>
2024-05-10 14:55:26 +0500

    feature: currency exchange service

--- bank/src/main/java/bank/Domain/CurrencyType.java
+++ bank/src/main/java/bank/Domain/CurrencyType.java
@@ -1,7 +1,13 @@
 package bank.Domain;
 
 public enum CurrencyType {
-    USD,
-    EUR,
-    RUB
+    RUB("643"),
+    USD("840"),
+    EUR("978");
+
+    private String code;
+    CurrencyType(String code){
+        this.code = code;
+    }
+    public String getCode(){ return code;}
 }
