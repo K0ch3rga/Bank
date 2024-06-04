3758cd5044f24303c4116dc6fea73bfa8b5f8a26
Toby Rice <mr.skel@mail.ru>
2024-06-04 21:12:52 +0500

    ready

--- bank/src/main/java/bank/Adapters/in/BaseController.java
+++ bank/src/main/java/bank/Adapters/in/BaseController.java
@@ -1,21 +1,35 @@
 package bank.Adapters.in;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import bank.Application.usecases.CustomerUsecase;
+import bank.Application.usecases.AccountUsecase;
 
 @Controller
-public class BaseController{
+public class BaseController {
+    AccountUsecase accountUsecase;
+
     @Autowired
-    CustomerUsecase customerUsecase;
+    public BaseController(AccountUsecase accountUsecase) {
+        this.accountUsecase = accountUsecase;
+    }
 
     @GetMapping("/status")
     @ResponseBody
     public String ok() {
         return "OK";
+    }
+
+    @GetMapping("/check/{id}")
+    @ResponseBody
+    public String checkBalance(long id) {
+        var acc = accountUsecase.getAccountById(id);
+        if (acc.isPresent()) {
+            return "Balance: " + String.valueOf(acc.get().balance());
+        } else {
+            return "No account";
+        }
     }
 }
